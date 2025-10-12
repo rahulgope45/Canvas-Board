7a3258089b4f84a7cdedb3e4610b84564f11e2bc
rahulgope45 <rahuleile456@gmail.com>
2025-10-12 21:30:28 +0530

    Canvas Bord is centered and styling is done

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,15 +4,16 @@ import { Canvas } from 'fabric';
 function Home() {
 
     const canvasRef = useRef(null);
-    const [canas , setCanvas] =useState(null);
+    const [canvas , setCanvas] =useState(null);
 
     useEffect(() =>{
         if(canvasRef.current){
             const initCanvas = new Canvas( canvasRef.current, {
                 width: 500,
                 height: 500,
+                
             });
-            initCanvas .backgroundColor = "#fff";
+            initCanvas.backgroundColor ='#fff';
             initCanvas.renderAll();
 
             setCanvas(initCanvas);
@@ -31,7 +32,11 @@ function Home() {
 
 
   return (
-    <div>
+    <div className='flex item-center justify-center mt-30'>
+        <div >
+            <canvas style={{ backgroundColor: "#ffffff" }}
+  className="border-2 border-gray-300 p-4 rounded-md" ref={canvasRef}/>
+        </div>
 
     </div>
   )
