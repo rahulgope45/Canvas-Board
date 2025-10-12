1c84d3cfe63993cbbe8ea350f5f8aab2b202021e
rahulgope45 <rahuleile456@gmail.com>
2025-10-12 21:59:57 +0530

    Aquare addition button added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,45 +1,61 @@
-import React,{useEffect, useRef , useState} from 'react'
+// @ts-nocheck
+
+import React, { useEffect, useRef, useState } from 'react'
 import { Canvas } from 'fabric';
+import { IconButton } from 'blocksin-system';
+import { SquareIcon } from 'sebikostudio-icons';
 
 function Home() {
 
     const canvasRef = useRef(null);
-    const [canvas , setCanvas] =useState(null);
+    const [canvas, setCanvas] = useState(null);
 
-    useEffect(() =>{
-        if(canvasRef.current){
-            const initCanvas = new Canvas( canvasRef.current, {
+    useEffect(() => {
+        if (canvasRef.current) {
+            const initCanvas = new Canvas(canvasRef.current, {
                 width: 500,
                 height: 500,
-                
+
             });
-            initCanvas.backgroundColor ='#fff';
+            initCanvas.backgroundColor = '#fff';
             initCanvas.renderAll();
 
             setCanvas(initCanvas);
 
-            return () =>{
+            return () => {
                 initCanvas.dispose();
 
             }
         }
 
-        
-       
 
-    },[])
+    }, []);
+
+    const addRectangle = () => {
+
+    }
 
 
 
-  return (
-    <div className='flex item-center justify-center mt-30'>
-        <div >
-            <canvas style={{ backgroundColor: "#ffffff" }}
-  className="border-2 border-gray-300 p-4 rounded-md" ref={canvasRef}/>
+    return (
+        <div className='flex  item-center justify-center mt-30'>
+            <div >
+                <canvas style={{ backgroundColor: "#ffffff" }}
+                    className="border-2 border-gray-300 p-4 rounded-md" ref={canvasRef} />
+            </div>
+
+            <div className="bg-gray-400 border border-gray-300 rounded-md p-2 hover:shadow-md transition">
+                <IconButton
+                    onClick={addRectangle}
+                    className="text-gray-700 hover:text-black-600 focus:outline-none"
+                    
+                >
+                    <SquareIcon />
+                </IconButton>
+            </div>
+
         </div>
-
-    </div>
-  )
+    )
 }
 
 export default Home
