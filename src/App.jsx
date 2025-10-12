6780be14e99fa6f744a38185f45986ff68428e71
rahulgope45 <rahuleile456@gmail.com>
2025-10-12 20:58:13 +0530

    Canvas Board Project Intialized with all the Packages

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1 className='text-red-600'>
+          Hello
+        </h1>
+        
+      </div>
+    
+    </>
+  )
+}
+
+export default App
